a0b67c26f4ac3f39b05606fec1c67e219da4af59
Alexei Barantsev <barancev@gmail.com>
2017-03-12 13:22:05 +0300

    Tests for Supplier

--- src/test/java/ru/stqa/trier/CounterBasedTrierTest.java
+++ src/test/java/ru/stqa/trier/CounterBasedTrierTest.java
@@ -21,6 +21,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -39,7 +41,46 @@ class CounterBasedTrierTest {
   }
 
   @Nested
-  @DisplayName("tries to run Runnable")
+  class BuildingTrier {
+
+    @Test
+    void shouldNotAllowToResetIgnoredExceptions() {
+      trier.ignoring(NumberFormatException.class);
+      assertThrows(IllegalStateException.class,
+        () -> trier.ignoring(NumberFormatException.class));
+    }
+
+    @Test
+    void shouldNotAllowToResetIgnoredResult() {
+      trier.ignoring(res -> res.equals("FAIL"));
+      assertThrows(IllegalStateException.class,
+        () -> trier.ignoring(res -> res.equals("FAIL")));
+    }
+
+    @Test
+    void shouldNotAllowToResetIgnoredResultWithUnexpectedResult() {
+      trier.ignoring(res -> res.equals("FAIL"));
+      assertThrows(IllegalStateException.class,
+        () -> trier.until(res -> res.equals("OK")));
+    }
+
+    @Test
+    void shouldNotAllowToResetUnexpectedResult() {
+      trier.until(res -> res.equals("OK"));
+      assertThrows(IllegalStateException.class,
+        () -> trier.until(res -> res.equals("OK")));
+    }
+
+    @Test
+    void shouldNotAllowToResetUnexpectedResultWithIgnoredResult() {
+      trier.until(res -> res.equals("OK"));
+      assertThrows(IllegalStateException.class,
+        () -> trier.ignoring(res -> res.equals("FAIL")));
+    }
+
+  }
+
+  @Nested
   class TryToRunRunnable {
 
     @Test
@@ -98,5 +139,130 @@ class CounterBasedTrierTest {
     }
   }
 
+  @Nested
+  class TryToRunSupplier {
+
+    @Test
+    void shouldReturnImmediatelyIfTheSupplierDoesNotThrow() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("OK");
+      assertThat(trier.tryTo(run), is("OK"));
+      verify(run, times(1)).get();
+      assertThat(clock.now(), is(0L));
+    }
+
+    @Test
+    void shouldIgnoreIgnoredExceptionThrownOnce() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class).thenReturn("OK");
+      assertThat(trier.ignoring(NumberFormatException.class).tryTo(run), is("OK"));
+      verify(run, times(2)).get();
+      assertThat(clock.now(), is(1L));
+    }
+
+    @Test
+    void shouldIgnoreIgnoredExceptionThrownSeveralTimes() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class).thenThrow(NumberFormatException.class).thenReturn("OK");
+      assertThat(trier.ignoring(NumberFormatException.class).tryTo(run), is("OK"));
+      verify(run, times(3)).get();
+      assertThat(clock.now(), is(2L));
+    }
+
+    @Test
+    void shouldIgnoreVariousIgnoredException() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class).thenThrow(ArrayIndexOutOfBoundsException.class).thenReturn("OK");
+      assertThat(trier.ignoring(NumberFormatException.class, ArrayIndexOutOfBoundsException.class).tryTo(run), is("OK"));
+      verify(run, times(3)).get();
+      assertThat(clock.now(), is(2L));
+    }
+
+    @Test
+    void shouldNotIgnoreIgnoredExceptionThrownAlways() {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class);
+      assertThrows(LimitExceededException.class,
+        () -> trier.ignoring(NumberFormatException.class).tryTo(run));
+      verify(run, times(5)).get();
+      assertThat(clock.now(), is(4L));
+    }
+
+    @Test
+    void shouldNotIgnoreNotIgnoredException() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class).thenThrow(ArrayIndexOutOfBoundsException.class).thenReturn("OK");
+      assertThrows(ArrayIndexOutOfBoundsException.class,
+        () -> trier.ignoring(NumberFormatException.class).tryTo(run));
+      verify(run, times(2)).get();
+      assertThat(clock.now(), is(1L));
+    }
+
+    @Test
+    void shouldIgnoreIgnoredResultReturnedOnce() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL").thenReturn("OK");
+      assertThat(trier.ignoring(res -> res.equals("FAIL")).tryTo(run), is("OK"));
+      verify(run, times(2)).get();
+      assertThat(clock.now(), is(1L));
+    }
+
+    @Test
+    void shouldIgnoreUnexpectedResultReturnedOnce() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL").thenReturn("OK");
+      assertThat(trier.until(res -> res.equals("OK")).tryTo(run), is("OK"));
+      verify(run, times(2)).get();
+      assertThat(clock.now(), is(1L));
+    }
+
+    @Test
+    void shouldIgnoreIgnoredResultReturnedSeveralTimes() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL").thenReturn("FAIL").thenReturn("OK");
+      assertThat(trier.ignoring(res -> res.equals("FAIL")).tryTo(run), is("OK"));
+      verify(run, times(3)).get();
+      assertThat(clock.now(), is(2L));
+    }
+
+    @Test
+    void shouldIgnoreUnexpectedResultReturnedSeveralTimes() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL1").thenReturn("FAIL2").thenReturn("OK");
+      assertThat(trier.until(res -> res.equals("OK")).tryTo(run), is("OK"));
+      verify(run, times(3)).get();
+      assertThat(clock.now(), is(2L));
+    }
+
+    @Test
+    void shouldIgnoreBothIgnoredExceptionAndResult() throws InterruptedException {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenThrow(NumberFormatException.class).thenReturn("FAIL").thenReturn("OK");
+      assertThat(trier.until(res -> res.equals("OK")).ignoring(NumberFormatException.class).tryTo(run), is("OK"));
+      verify(run, times(3)).get();
+      assertThat(clock.now(), is(2L));
+    }
+
+    @Test
+    void shouldNotIgnoreIgnoredResultReturnedAlways() {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL");
+      assertThrows(LimitExceededException.class,
+        () -> trier.ignoring(res -> res.equals("FAIL")).tryTo(run));
+      verify(run, times(5)).get();
+      assertThat(clock.now(), is(4L));
+    }
+
+    @Test
+    void shouldNotIgnoreUnexpectedResultReturnedAlways() {
+      Supplier run = mock(Supplier.class);
+      when(run.get()).thenReturn("FAIL");
+      assertThrows(LimitExceededException.class,
+        () -> trier.until(res -> res.equals("OK")).tryTo(run));
+      verify(run, times(5)).get();
+      assertThat(clock.now(), is(4L));
+    }
+
+  }
 
 }
